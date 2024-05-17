93d12b3655d3eb84f874cfc2ad2bfd49dcce3d08
proabdulbasit <proabdulbasit.me@gmail.com>
2024-05-17 16:28:54 +0500

    update code

---
+++ out/_next/static/BF5HzRfgAtu0n8gMX1B29/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/185-d0e0d5d592283ebc.js","static/css/4df78f2cd73d6b26.css","static/chunks/pages/index-37b7ad251bcde3fb.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
